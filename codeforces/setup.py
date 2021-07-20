8a96edb04931f0984e87c8de5368de855a33047a
diggi55 <schaeffer-tobias@gmx.de>
2021-07-20 20:25:07 +0200

    Minor changes

--- codeforces/setup.py
+++ codeforces/setup.py
@@ -20,11 +20,13 @@ def main():
 
     if sys.argv[1] in practice:
         dir_name = "practice/"
+        # test if .py is already file ending / another file ending was is used
         fl_name = sys.argv[2] + ".py"
         shutil.copyfile(wf, dir_name + fl_name)
     elif sys.argv[1] in contest:
         trnmt_name = sys.argv[2].replace(" ", "_") + "/"
         dir_name = "contest/" + trnmt_name
+        # only if directory isn't already present
         os.mkdir(dir_name)
         count = int(sys.argv[3])
 
