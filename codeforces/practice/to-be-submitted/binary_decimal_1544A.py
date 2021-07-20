ad51ea59fc1e76a820b08c2ba30ac9606427ed3f
diggi55 <schaeffer-tobias@gmx.de>
2021-07-20 20:24:51 +0200

    Practice

---
+++ codeforces/practice/to-be-submitted/binary_decimal_1544A.py
@@ -0,0 +1,14 @@
+# Accepted:
+
+# import math as m
+# import itertools as it
+
+def main():
+    t = int(input())
+
+    for i in range(t):
+        print(max(list(input())))
+
+
+if __name__ == "__main__":
+    main()
